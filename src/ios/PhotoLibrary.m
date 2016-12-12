e4d4be78603ab5555c3fcb3c5eee17390700f51f
chadhelbling <chad@coschedule.com>
2016-12-12 16:54:01 -0600

    [FEATURE] Reformatted video vs image saving

--- src/ios/PhotoLibrary.m
+++ src/ios/PhotoLibrary.m
@@ -10,7 +10,7 @@
 @synthesize localId;
 
 
-- (void)insertImage:(UIImage *)image intoAlbumNamed:(NSString *)albumName {
+- (void)insertVideoUrl:(NSURL*)videoUrl insertImage:(UIImage *)image intoAlbumNamed:(NSString *)albumName {
     //Fetch a collection in the photos library that has the title "albumName"
     PHAssetCollection *collection = [self fetchAssetCollectionWithAlbumName: albumName];
 
@@ -20,19 +20,19 @@
             [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle: albumName];
         } completionHandler:^(BOOL success, NSError * _Nullable error) {
             if (error != nil) {
-                NSLog(@"Error inserting image into album: %@", error.localizedDescription);
+                NSLog(@"Error inserting media into album: %@", error.localizedDescription);
                 [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
             }
 
             if (success) {
                 //Fetch the newly created collection (which we *assume* exists here)
                 PHAssetCollection *newCollection = [self fetchAssetCollectionWithAlbumName:albumName];
-               [self insertImage:image intoAssetCollection: newCollection];
+                [self insertVideoUrl:videoUrl insertImage:image intoAssetCollection: newCollection];
             }
         }];
     } else {
         //If we found the existing AssetCollection with the title "albumName", insert into it
-         [self insertImage:image intoAssetCollection: collection];
+        [self insertVideoUrl:videoUrl insertImage:image intoAssetCollection: collection];
     }
 }
 
@@ -48,11 +48,15 @@
     return fetchResult.firstObject;
 }
 
-- (void)insertImage:(UIImage *)image intoAssetCollection:(PHAssetCollection *)collection {
+- (void)insertVideoUrl:(NSURL *)videoUrl insertImage:(UIImage *)image intoAssetCollection:(PHAssetCollection *)collection {
     [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
-
-        //This will request a PHAsset be created for the UIImage
-        PHAssetCreationRequest *creationRequest = [PHAssetCreationRequest creationRequestForAssetFromImage:image];
+        __block PHAssetCreationRequest *creationRequest;
+        //This will request a PHAsset be created for the URL
+        if(videoUrl != nil) {
+            creationRequest = [PHAssetCreationRequest creationRequestForAssetFromVideoAtFileURL:videoUrl];
+        } else {
+            creationRequest = [PHAssetCreationRequest creationRequestForAssetFromImage:image];
+        }
 
         //Create a change request to insert the new PHAsset in the collection
         PHAssetCollectionChangeRequest *request = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:collection];
@@ -90,44 +94,8 @@
     NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:url]];
     UIImage *image = [UIImage imageWithData:data];
 
-    [self insertImage:image intoAlbumNamed: @"My Album"];
+    [self insertVideoUrl:nil insertImage:image intoAlbumNamed: @"My Album" ];
 
-
-
-//    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
-//    [library addAssetsGroupAlbumWithName:@"My Photo Album" resultBlock:^(ALAssetsGroup *group) {
-//        if(group == nil){
-//                //enumerate albums
-//                [library enumerateGroupsWithTypes:ALAssetsGroupAlbum
-//                                   usingBlock:^(ALAssetsGroup *g, BOOL *stop)
-//                 {
-//                     //if the album is equal to our album
-//                     if ([[g valueForProperty:ALAssetsGroupPropertyName] isEqualToString:@"My Photo Album"]) {
-//
-//                         //save image
-//                        [library writeImageDataToSavedPhotosAlbum:data metadata:nil completionBlock:^(NSURL *assetURL, NSError *error) {
-//                            NSString *assetUrlString = assetURL.absoluteString;
-//                            CDVPluginResult *result = error == NULL
-//                                ? [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:assetUrlString]
-//                                : [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
-//
-//                            [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
-//                        }];
-//                     }
-//                 }failureBlock:^(NSError *error){
-//
-//                 }];
-//        }else {
-//            [library writeImageDataToSavedPhotosAlbum:data metadata:nil completionBlock:^(NSURL *assetURL, NSError *error) {
-//                NSString *assetUrlString = assetURL.absoluteString;
-//                CDVPluginResult *result = error == NULL
-//                    ? [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:assetUrlString]
-//                    : [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
-//
-//                [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
-//            }];
-//        }
-//    }];
 
 }
 
@@ -141,22 +109,8 @@
     [data writeToFile:path atomically:YES];
     NSURL *pathUrl = [NSURL fileURLWithPath:path];
 
-    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
-    if ([library videoAtPathIsCompatibleWithSavedPhotosAlbum:pathUrl]) {
-        [library writeVideoAtPathToSavedPhotosAlbum:pathUrl completionBlock:^(NSURL *assetURL, NSError *error) {
-            [[NSFileManager defaultManager] removeItemAtPath:path error:NULL];
-            NSString *assetUrlString = assetURL.absoluteString;
-            CDVPluginResult *result = error == NULL
-                ? [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:assetUrlString]
-                : [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
+    [self insertVideoUrl:pathUrl insertImage:nil intoAlbumNamed: @"My Album"];
 
-            [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
-        }];
-    }
-    else {
-        CDVPluginResult *result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:@"Incompatible format"];
-        [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
-    }
 }
 
 - (void)dealloc
